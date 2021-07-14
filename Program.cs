c22ec6d385a2477e70ede653beec41fb9cb7667a
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-14 19:19:40 +0530

    Pooja Added:Welcome to Line Comparison Problem

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparison_Oops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Welcome to the Line Comparison Program!");
+            // UC1.getCartesian();
+            //  UC2.getCartesian();
+            // UC3.getCartesian();
+            UC4 obj = new UC4();
+            obj.input();
+            obj.cal();
+            obj.CompareLines();
+
+
+        }
+    }
+}
+    
+
